400fac0dbbe3620889381f0e6e88932125c7b1a4
dawiddc <dawid.ciok@gmail.com>
2018-12-02 21:43:51 +0100

    ProductDaoImpl refactor

--- src/main/java/com/ciokoko/dao/ProductDaoImpl.java
+++ src/main/java/com/ciokoko/dao/ProductDaoImpl.java
@@ -20,33 +20,42 @@ import java.util.Map;
 @Slf4j
 public class ProductDaoImpl implements ProductDao {
 
+    private Iterable<CSVRecord> records;
+
     @Override
     public List<Product> getProducts() {
         List<Product> products = new ArrayList<>();
-        Iterable<CSVRecord> records;
         try (Reader in = new FileReader(new ClassPathResource("products-consumption.csv").getFile())) {
             records = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
             for (CSVRecord record : records) {
-                Map<Integer, Double> valuesPerYear = new HashMap<>();
-                for (int i = 2; i < record.size(); i++) {
-                    Integer year = 0;
-                    for (Map.Entry<String, Integer> entry : ((CSVParser) records).getHeaderMap().entrySet()) {
-                        if (entry.getValue() == i) {
-                            year = Integer.valueOf(entry.getKey());
-                        }
-                    }
-                    valuesPerYear.put(year, Double.valueOf(record.get(i)));
-                }
-                final Product product = Product.builder()
-                        .name(record.get("product"))
-                        .unit(record.get("unit"))
-                        .valuesPerYear(valuesPerYear)
-                        .build();
+                final Product product = buildProduct(record);
                 products.add(product);
             }
         } catch (IOException e) {
             log.error("Error reading file!");
         }
         return products;
+    }
+
+    private Product buildProduct(CSVRecord record) {
+        return Product.builder()
+                .name(record.get("product"))
+                .unit(record.get("unit"))
+                .valuesPerYear(buildValuesPerYear(record))
+                .build();
+    }
+
+    private Map<Integer, Double> buildValuesPerYear(CSVRecord record) {
+        Map<Integer, Double> valuesPerYear = new HashMap<>();
+        for (int i = 2; i < record.size(); i++) {
+            Integer year = 0;
+            for (Map.Entry<String, Integer> entry : ((CSVParser) records).getHeaderMap().entrySet()) {
+                if (entry.getValue() == i) {
+                    year = Integer.valueOf(entry.getKey());
+                }
+            }
+            valuesPerYear.put(year, Double.valueOf(record.get(i)));
+        }
+        return valuesPerYear;
     }
 }
