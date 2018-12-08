163c03f502fe51dbb3a85894e288486c153947e2
dawiddc <dawid.ciok@gmail.com>
2018-12-08 19:52:16 +0100

    Add chart labels

--- frontend/product-consumption-charts/src/app/chart.component.ts
+++ frontend/product-consumption-charts/src/app/chart.component.ts
@@ -19,15 +19,19 @@ export class ChartComponent implements OnInit {
   ngOnInit() {
     this.chartDataService.getProductData().subscribe((products) => {
       this.products = products;
-      for (const product of this.products) {
-        for (const key of Object.keys(product.valuesPerYear)) {
-          if (!this.allYears.includes(Number(key))) {
-            this.allYears.push(Number(key));
-          }
-        }
-      }
+      this.prepareYearList();
       this.prepareChart();
     });
+  }
+
+  private prepareYearList() {
+    for (const product of this.products) {
+      for (const key of Object.keys(product.valuesPerYear)) {
+        if (!this.allYears.includes(Number(key))) {
+          this.allYears.push(Number(key));
+        }
+      }
+    }
   }
 
   private prepareChart() {
@@ -37,20 +41,34 @@ export class ChartComponent implements OnInit {
         labels: this.allYears,
         datasets: [
           {
+            label: this.products[0].name,
             data: Object.values(this.products[0].valuesPerYear),
             borderColor: '#3cba9f',
             fill: false
           },
           {
+            label: this.products[1].name,
             data: Object.values(this.products[1].valuesPerYear),
             borderColor: '#ffcc00',
+            fill: false
+          },
+          {
+            label: this.products[2].name,
+            data: Object.values(this.products[2].valuesPerYear),
+            borderColor: '#fa92ff',
+            fill: false
+          },
+          {
+            label: this.products[3].name,
+            data: Object.values(this.products[3].valuesPerYear),
+            borderColor: '#ec8d67',
             fill: false
           },
         ]
       },
       options: {
         legend: {
-          display: false
+          display: true
         },
         scales: {
           xAxes: [{
